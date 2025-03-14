053a7c907255c69742b795c43d9d199701740f4e
Daniel Vera Nieto <daveni@protonmail.com>
2025-03-14 17:09:34 +0100

    Update.

--- a3/assignment3a.py
+++ a3/assignment3a.py
@@ -14,4 +14,22 @@ local_rank = int(os.environ["SLURM_LOCALID"])
 # Read the information form the node
 hostname = socket.gethostname()
 # Each process prints a final message to confirm it didn't get stuck
-print(f"[Python] rank={rank} | host={hostname} | {master_addr}:{master_port} | {world_size} | {foobar} ")
+print(f"[Python] rank={rank} | host={hostname} | {master_addr}:{master_port} | {world_size} | {foobar} ")   
+
+# Initializes the default (global) process group
+dist.init_process_group(
+backend="nccl", # the NCCL backend requires a GPU on each process
+init_method=f"tcp://{master_addr}:{master_port}",
+world_size=world_size,
+rank=rank,
+)
+# Limit GPU allocation of this process to only one GPU
+torch.cuda.set_device(local_rank)
+# Create a float32 tensor on each rank with a single element of value 'rank' and move it to the GPU.
+local_tensor = torch.tensor([rank], dtype=torch.float32).cuda()
+print(f"[Python] rank={rank} | local_rank={local_rank} | host={hostname} | local_tensor={local_tensor.item()}")
+# Perform a sum operation across all ranks.
+dist.all_reduce(local_tensor, op=dist.ReduceOp.SUM)
+print(f"[Python] rank={rank} | local_rank={local_rank} | host={hostname} | local_tensor_after_all_reduce={local_tensor.item()}")
+# Cleanup
+dist.destroy_process_group()
