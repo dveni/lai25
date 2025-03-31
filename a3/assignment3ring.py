8b1d48790d18ad24fc520f00233ddf6bd1d5dc92
Daniel Vera Nieto <daveni@protonmail.com>
2025-03-31 21:57:55 +0200

    Update.

--- a3/assignment3ring.py
+++ a3/assignment3ring.py
@@ -34,31 +34,59 @@ send_tensor = torch.full((N,), fill_value=rank, dtype=torch.float32, device="cud
 # Prepare a tensor to receive data
 recv_tensor = torch.zeros(N, dtype=torch.float32, device="cuda")
 
+# # Synchronize before starting communication
+# dist.barrier()
+# # Start send and receive
+# send_start = time.time()
+# # print(f"[Python] rank={rank} | Sending data to rank={send_rank}")
+# # send_req = dist.isend(tensor=send_tensor, dst=send_rank)
+# # print(f"[Python] rank={rank} | Receiving data from rank={recv_rank}")
+# # recv_req = dist.irecv(tensor=recv_tensor, src=recv_rank)
+# # # Wait for both send and receive to complete
+# # print(f"[Python] rank={rank} | Waiting for send_req and recv_req to complete")
+# if rank % 2 == 0:
+#     send_req = dist.send(tensor=send_tensor, dst=send_rank)
+#     recv_req = dist.recv(tensor=recv_tensor, src=recv_rank)
+# else:
+#     recv_req = dist.recv(tensor=recv_tensor, src=recv_rank)
+#     send_req = dist.send(tensor=send_tensor, dst=send_rank)
+# send_req.wait()
+# recv_req.wait()
+
+# torch.cuda.synchronize() # shouldn't be needed but .wait() is not behaving as expected.
+# print(f"[Python] rank={rank} is_complete={send_req.is_completed()}", flush=True)
+# print(f"[Python] rank={rank} is_complete={recv_req.is_completed()}", flush=True)
+
+# send_end = time.time()
+# elapsed_seconds = send_end - send_start
+
+# warmup
+for _ in range(5):
+  if rank % 2 == 0:
+    send_req = dist.send(tensor=send_tensor, dst=send_rank)
+    recv_req = dist.recv(tensor=recv_tensor, src=recv_rank)
+  else:
+    recv_req = dist.recv(tensor=recv_tensor, src=recv_rank)
+    send_req = dist.send(tensor=send_tensor, dst=send_rank)
+
 # Synchronize before starting communication
+torch.cuda.synchronize()
+# Wait for other nodes
 dist.barrier()
+
 # Start send and receive
 send_start = time.time()
-# print(f"[Python] rank={rank} | Sending data to rank={send_rank}")
-# send_req = dist.isend(tensor=send_tensor, dst=send_rank)
-# print(f"[Python] rank={rank} | Receiving data from rank={recv_rank}")
-# recv_req = dist.irecv(tensor=recv_tensor, src=recv_rank)
-# # Wait for both send and receive to complete
-# print(f"[Python] rank={rank} | Waiting for send_req and recv_req to complete")
+
 if rank % 2 == 0:
-    send_req = dist.send(tensor=send_tensor, dst=send_rank)
-    recv_req = dist.recv(tensor=recv_tensor, src=recv_rank)
+  send_req = dist.send(tensor=send_tensor, dst=send_rank)
+  recv_req = dist.recv(tensor=recv_tensor, src=recv_rank)
 else:
-    recv_req = dist.recv(tensor=recv_tensor, src=recv_rank)
-    send_req = dist.send(tensor=send_tensor, dst=send_rank)
-send_req.wait()
-recv_req.wait()
+  recv_req = dist.recv(tensor=recv_tensor, src=recv_rank)
+  send_req = dist.send(tensor=send_tensor, dst=send_rank)
 
-print(f"[Python] rank={rank} is_complete={send_req.is_completed()}", flush=True)
-print(f"[Python] rank={rank} is_complete={recv_req.is_completed()}", flush=True)
-torch.cuda.synchronize() # shouldn't be needed but .wait() is not behaving as expected.
-
-send_end = time.time()
-elapsed_seconds = send_end - send_start
+# Wait for both send and receive to complete
+torch.cuda.synchronize()
+elapsed_seconds = time.time() - send_start
 
 
 
