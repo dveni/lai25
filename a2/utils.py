c085282b4e54308fd389ff6e6be2a7c24c6e4dec
Daniel Vera Nieto <daveni@protonmail.com>
2025-05-24 23:49:44 +0200

    Update.

--- a2/utils.py
+++ a2/utils.py
@@ -17,7 +17,25 @@ PRECISION_STR_TO_DTYPE = {
     "fp32": torch.float32,
     "fp64": torch.float64,
 }
+def inspect_model(model: FSDPModule):
+    assert isinstance(model, Transformer)
+    assert isinstance(model, FSDPModule)
 
+    if torch.distributed.get_rank() == 0:
+        print(model)
+
+    for param in model.parameters():
+        assert param.placements == (Shard(0),)
+        assert param.dtype == torch.float32
+        # print(param.get_local_tensor())
+
+
+def inspect_mixed_precision(model: FSDPModule):
+    model.unshard()
+    for param in model.parameters(recurse=False):
+        assert param.dtype == torch.bfloat16
+    model.reshard()
+    
 def init_distributed():
     """
     Initialise the distributed environment.
