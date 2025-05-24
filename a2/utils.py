3ebb089f1409fa58c8eed7c2587aab9533b44fa5
Daniel Vera Nieto <daveni@protonmail.com>
2025-05-25 00:02:07 +0200

    Update.

--- a2/utils.py
+++ a2/utils.py
@@ -30,15 +30,15 @@ def inspect_model(model: FSDPModule):
 
     for param in model.parameters():
         assert param.placements == (Shard(0),)
-        assert param.dtype == torch.float32
+        assert param.dtype == torch.torch.bfloat16
         # print(param.get_local_tensor())
 
 
-def inspect_mixed_precision(model: FSDPModule):
-    model.unshard()
-    for param in model.parameters(recurse=False):
-        assert param.dtype == torch.bfloat16
-    model.reshard()
+# def inspect_mixed_precision(model: FSDPModule):
+#     model.unshard()
+#     for param in model.parameters(recurse=False):
+#         assert param.dtype == torch.bfloat16
+#     model.reshard()
 
 def init_distributed():
     """
